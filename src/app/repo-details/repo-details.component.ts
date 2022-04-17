c37683678eefe9eb027831b97581f6d611fade21
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-04-17 17:56:43 +0300

    ft: I have used inputy property binding so that i can use another component to display my repo results

--- src/app/repo-details/repo-details.component.ts
+++ src/app/repo-details/repo-details.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Repos } from '../repos';
 @Component({
   selector: 'app-repo-details',
   templateUrl: './repo-details.component.html',
   styleUrls: ['./repo-details.component.css']
 })
 export class RepoDetailsComponent implements OnInit {
-
+@Input() repo:Repos[];
   constructor() { }
 
   ngOnInit(): void {
