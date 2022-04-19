efb300b8e0cc17103949ca9f56a960cf441f79aa
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-04-19 16:01:10 +0300

    ft: Custom Date count pipe is functional

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let valueArr = value.replace(/(\d{4})-(\d{2})-(\d{2}).*/ig,"$1 $2 $3").split(" ");
+    let createdAt = new Date(valueArr[0],valueArr[1]-1, valueArr[2]);
+    let now = new Date();
+    let count = now.getTime() - createdAt.getTime();
+    count=count/(1000 * 60 * 60 * 24);
+    return Math.floor(count);
   }
 
 }
